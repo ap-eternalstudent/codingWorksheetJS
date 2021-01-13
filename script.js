25b5b0a1d3db5ec16de9356bf8c5fc8cd9bb010f
anthony28 <anthony@wearea2.com>
2021-01-13 17:27:39 -0500

    added array sum function

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ userInputNumber = Number(prompt("give me a number"));
 userInputString = String(prompt("and now give me a word"));
 
 someFunc(userInputNumber, userInputString);
+// Please create a function that takes in an array. The function should console.log() the sum of the first value in the array and the last value in the array
+
+myArray = [1, 2, 3, 4, 5, 6, 7];
+
+const arrayVariableSum = function (array) {
+  fisrtVar = array[0];
+  secondVar = array.length - 1;
+  return fisrtVar + secondVar;
+};
+
+arrayVariableSum(myArray);
