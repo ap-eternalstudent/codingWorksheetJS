47d64c50fdba57c9b22669d17f72fcab5fbb4cb6
anthony28 <anthony@wearea2.com>
2021-01-15 11:34:06 -0500

    added challenge #3

--- script.js
+++ script.js
@@ -32,3 +32,5 @@ const takesName = function (name) {
 takesName("Anton");
 takesName("George");
 takesName("Boris");
+
+// Please create a function that takes in an array of numbers and returns a new array that only contains odd numbers.
